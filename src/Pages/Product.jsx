da00717f75fa1105887bda42a096e2af52c61d95
unknown <Breke64@gmail.com>
2025-02-10 13:39:47 -0500

    added html structure for project display component and added to product page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,5 +1,18 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext.jsx";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum.jsx";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay.jsx";
 const Product = () => {
-  return <div></div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+  return (
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
 };
 
 export default Product;
