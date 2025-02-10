32a1a47e258cd0d8480f99917a3aefc7ee75e817
unknown <Breke64@gmail.com>
2025-02-09 23:55:20 -0500

    loginsignup client display page completed

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import "./CSS/loginSignup.css";
 const LoginSignup = () => {
-  return <div></div>;
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already Have and account? <span>Login Here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>
+            By continuing I agree to use the terms of use & privacy policy.{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default LoginSignup;
