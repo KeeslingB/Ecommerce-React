b70dd144cc8705dd9326f146abe39c89eafa40aa
unknown <Breke64@gmail.com>
2025-02-12 23:38:00 -0500

    breadcrum component html structure - prop handling

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -6,7 +6,7 @@ const Breadcrum = (props) => {
   return (
     <div className="breadcrum">
       HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" />
-      {product.category} <img src={arrow_icon} alt="" /> {product.name}
+      {props.category} <img src={arrow_icon} alt="" /> {props.name}
     </div>
   );
 };
